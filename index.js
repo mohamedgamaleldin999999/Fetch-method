61c4dd9ee4456ed2438fe3a69a281ed6e72a6fe1
mohamedgamaleldin999999 <123296170+mohamedgamaleldin999999@users.noreply.github.com>
2023-08-09 01:26:50 +0300

    Finish video but buggy

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 let url = 'http://jsonplaceholder.typicode.com/comments?postId=42';
 
-fetch(url);
+fetch(url)
+.then(response => response.json() )
+.then(data => {
+  let str = JSON.stringfy(data, null, '\t')
+  document.querySelector('#output pre').textContent = str;
+})
+.catch(err => {
+  let nm = err.name;
+  let msg = err.message;
+  alert('CATCH: ${nm} ${msg}')
+});
